c9de4cf9c439585460f359c26372612a10af88f6
arsensag <arsensag@mail.ru>
2017-01-21 19:08:25 +0300

    AI added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
     public float lifeTime = 5.9f;
-	void Start () {
-		
-	}
+    public GameObject path;
+
+    private Queue<Vector3> dots;
+    private Vector3 next_pos;
+    public float speed = 5;
+
+    void Start () {
+        Vector3 spawnPosition = new Vector3(Random.Range(-19f, 19f), 0f, Random.Range(-19f, 19f));
+        transform.position = spawnPosition;
+
+        foreach (Transform child in path.transform)
+        {
+            dots.Enqueue(child.transform.position);
+        }
+        Debug.Log(dots);
+        next_pos = dots.Dequeue();
+    }
 	
 	void Update () {
-        lifeTime -= Time.deltaTime;
-        if (lifeTime < 0) Destroy(gameObject);
-        Vector3 spawnPosition = new Vector3(Random.Range(-19f, 19f), 0f, Random.Range(-19f, 19f));
+        Destroy(this);
+        Debug.Log(dots);
 
-        transform.position = spawnPosition;
+        Vector3 curr_pos = transform.position;
+
+        transform.position = Vector3.Lerp(curr_pos, next_pos, speed);
+
+        if (next_pos==curr_pos)
+        {
+            if (dots.Count==0)
+            {
+                Destroy(this);
+            }
+            next_pos = dots.Dequeue();
+        }
     }
 }
---
+++ Assets/Scripts/myFPSController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class myFPSController : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float turnSpeed = 5f;
+    //public Gun gun;
+    public int playerNumber;
+    public float sensitivity = 5.0f;
+
+    void Start () {
+	    Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    void Update()
+    {
+        float forwardMove = Input.GetAxis("Vertical") * turnSpeed * Time.deltaTime;
+        float strafe = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(strafe, 0, forwardMove);
+
+        /*float isFired = Input.GetAxis("Fire1_p" + playerNumber);
+        if (isFired > 0 && gun.isReadyToShoot())
+            gun.Fire();
+            */
+
+        float mouseH = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseV = Input.GetAxis("Mouse Y") * sensitivity;
+
+        Camera.main.transform.Rotate(-mouseV,0,0);
+        this.transform.Rotate(0, mouseH, 0);
+
+    }
+}
+
